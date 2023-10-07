57a43baf4403a02cbb94b040f1f9de95e5b36f16
kariSally <sallykarisaf@gmail.com>
2023-10-07 04:39:43 -0700

    C variables, if, else, while project

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - it prints numbers btn 0 to 9 and letters btwn a to f.
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
